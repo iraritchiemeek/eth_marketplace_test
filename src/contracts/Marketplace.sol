569fcfa9e39013113865c6fefae9498f056045e9
Ira Ritchie Meek <iraritchiemeek@gmail.com>
2021-07-03 09:43:01 +1200

    add product and createproduct fn

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,8 +2,34 @@ pragma solidity ^0.5.0;
 
 contract Marketplace {
 	string public name;
+	uint public productCount = 0;
+	mapping(uint => Product) public products;
+
+	struct Product {
+		uint id;
+		string name;
+		uint price;
+		address owner;
+		bool purchased;
+	}
+
+	event ProductCreated(
+	    uint id,
+	    string name,
+	    uint price,
+	    address owner,
+	    bool purchased
+	);
 
 	constructor() public {
 		name = "Test Marketplace";
+	}
+
+	function createProduct(string memory _name, uint _price) public {
+		require(bytes(_name).length > 0);
+		require(_price > 0);
+		productCount++;
+		products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+		emit ProductCreated(productCount, _name, _price, msg.sender, false);
 	}
 }
